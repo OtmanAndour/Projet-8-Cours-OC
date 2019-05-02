87c521b475cc64e0f1459da20f51cec9f6556df9
Othman Andour <andour.otman@gmail.com>
2019-05-02 19:34:31 +0200

    Created my first template!

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     re_path(r'^article/(?P<id_article>.+)', views.view_article, name="afficher_article"),
     re_path(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})', views.list_articles),
     path('redirection', views.view_redirection),
+    path('date', views.date_actuelle),
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
 ]
--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, Http404
+from datetime import datetime
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -23,3 +24,12 @@ def view_redirection(request):
 def list_articles(request, month, year):
     # Il veut des articles ? Soyons fourbe et redirigeons-le vers djangoproject.com
     return redirect("https://www.djangoproject.com")
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+def addition(request, nombre1, nombre2):
+    total = nombre1 + nombre2
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
