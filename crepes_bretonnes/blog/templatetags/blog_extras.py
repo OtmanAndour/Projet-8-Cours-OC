c6dedc2d42aca35c952456af707ad661dc80341a
Othman Andour <andour.otman@gmail.com>
2019-05-07 16:20:07 +0200

    Introductions to signals and middlewares

--- crepes_bretonnes/blog/templatetags/blog_extras.py
+++ crepes_bretonnes/blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
+from random import randint
 
 register = template.Library()
 
@@ -48,3 +49,36 @@ def smart_truncate(texte, nb_caracteres=20):
         texte = texte[0:-1]
 
     return texte + '…'
+
+
+@register.tag
+def random(parser, token):
+    """ Tag générant un nombre aléatoire, entre les bornes données en arguments """
+    try:
+        nom_tag, begin, end = token.split_contents()
+    except ValueError:
+        msg = 'Le tag %s doit prendre exactement deux arguments.' % token.split_contents()[0]
+        raise template.TemplateSyntaxError(msg)
+
+    # Nous vérifions que nos deux paramètres sont bien des entiers
+    try:
+        begin, end = int(begin), int(end)
+    except ValueError:
+        msg = 'Les arguments du tag %s sont obligatoirement des entiers.' % nom_tag
+        raise template.TemplateSyntaxError(msg)
+
+    # Nous vérifions si le premier est bien inférieur au second
+    if begin > end:
+        msg = 'L\'argument "begin" doit obligatoirement être inférieur à l\'argument "end" dans le tag %s.' % nom_tag
+        raise template.TemplateSyntaxError(msg)
+
+    return RandomNode(begin, end)
+
+
+class RandomNode(template.Node):
+    def __init__(self, begin, end):
+        self.begin = begin
+        self.end = end
+
+    def render(self, context):
+        return ("Un nombre aléatoire entre " + str(self.begin) + " et " + str(self.end) + " est: " + str(randint(self.begin, self.end)))
--- crepes_bretonnes/crepes_bretonnes/settings.py
+++ crepes_bretonnes/crepes_bretonnes/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'mini_url',
-    'classe'
+    'classe',
+    'stats',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'crepes_bretonnes.context_processors.get_infos',
             ],
         },
     },
